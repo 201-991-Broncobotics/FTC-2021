432789e99494942393eb1bec685a28656cca5946
NGeorge25 <ngeorge23@brophybroncos.org>
2022-02-18 13:34:15 -0700

    Post-Rollback

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Duckwheel_Blue_Auton_V2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Duckwheel_Blue_Auton_V2.java
@@ -1,14 +1,13 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Systems.RobotHardware;
-// @Disabled
-@Autonomous(name = "Blue Duckwheel Auton V2")
+
+@Autonomous(name = "Blue Duckwwheel Auton (V2)")
 public class Duckwheel_Blue_Auton_V2 extends LinearOpMode implements Auton_Values{
 
     private ElapsedTime runtime = new ElapsedTime();
@@ -23,47 +22,40 @@ public class Duckwheel_Blue_Auton_V2 extends LinearOpMode implements Auton_Value
         while (opModeIsActive()) {
 
             //checking position and go to set position by first square
-            Drive(3.5); //see if it works
-
-            sleep(10000);
-
+            Drive(7.5);
             checkPos(3);
             if(elementPosition == 3) { //if we have the thingy at the third square
-                Drive(distance_between_squares*2, "Left", 0.3);
+                Drive(distance_between_squares*2, "Left");
             } else {
-                Drive(distance_between_squares, "Left", 0.3);
+                Drive(distance_between_squares, "Left");
                 checkPos(2);
-                Drive(distance_between_squares, "Left", 0.3);
+                Drive(distance_between_squares, "Left");
             } //elementPosition = element position; ends up by square
             robot.telemetry.addData("Barcode: ", elementPosition);
             robot.telemetry.update();
 
-            sleep(1000);
-
             //drop block in tower
-            Drive(8, "Left", 0.3);
-            Drive(3);
+            Drive(8, "Left");
+            Drive(3.5);
 
-            robot.IN.setPower(0.5);
+            robot.IN.setPower(0.4);
             robot.Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             SetArm(0);
             robot.rServo.setPosition(servoM);
-          //  robot.lServo.setPosition(1-servoM);
+
             robot.IN.setPower(0);
             sleep(100);
-
-            
             SetArm(elementPosition);
             robot.rServo.setPosition(servoBM);
-         //   robot.lServo.setPosition(1-servoBM);
+
             sleep(100);
             robot.rServo.setPosition(servoD);
-         //   robot.lServo.setPosition(1-servoD);
+
             sleep(2000);
 
             //reset arm
             robot.rServo.setPosition(servoB);
-          //  robot.lServo.setPosition(1-servoB);
+
             SetArm(0);
             sleep(1000);
 
@@ -95,12 +87,7 @@ public class Duckwheel_Blue_Auton_V2 extends LinearOpMode implements Auton_Value
     private void ExecuteEncoders() {
         robot.SpeedSet(0.7);
         while (robot.MotorsBusy() && opModeIsActive()) {
-            robot.telemetry.addData("Encoder RF: ", robot.RF.getCurrentPosition());
-            robot.telemetry.addData("Encoder LF: ", robot.LF.getCurrentPosition());
-            robot.telemetry.addData("Encoder RB: ", robot.RB.getCurrentPosition());
-            robot.telemetry.addData("Encoder LB: ", robot.LB.getCurrentPosition());
-
-            robot.telemetry.update();
+            idle();
         }
         robot.SpeedSet(0.2);
         sleep(100);
@@ -120,7 +107,7 @@ public class Duckwheel_Blue_Auton_V2 extends LinearOpMode implements Auton_Value
 
     private void checkPos(int position){
         //robot.getGreen() > 100 &&
-        if(robot.getDistInch() < 8){
+        if(robot.getDistInch() < 4){
             elementPosition = position;
         }
     }
@@ -137,11 +124,6 @@ public class Duckwheel_Blue_Auton_V2 extends LinearOpMode implements Auton_Value
     private void Drive(double Dist, String Direction, double Speed){
         robot.DriveDistance(Dist, Direction);
         ExecuteEncoders(Speed);
-    }
-
-    private void ShaanDrive(double Dist) {
-        robot.ShaanDriveDistance(-Dist);
-        ExecuteEncoders();
     }
 
     private void Turn(int Degrees, String Direction){
